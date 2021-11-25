abd869c56a143f15d154177040af9e6252d8bf70
freddymuleya16 <freddymuleya16@gmail.com>
2021-11-25 17:11:39 +0200

    Profile picture

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,24 +3,22 @@ import React from 'react';
 import { Section, SectionText, SectionTitle } from '@styles/GlobalComponents';
 import Button from '@styles/GlobalComponents/Button';
 import { HeroImg, LeftSection } from './HeroStyles';
-import ProfilePicture from './ProfilePicture.jpg';
 
 const Hero = (props) => (
   <Section row nopadding>
     <LeftSection>
-      <HeroImg src={"./ProfilePicture.jpg"}/>
+      <HeroImg src={"Assets/Img/ProfilePicture.jpg"} />
       <SectionTitle main center>
         I am Freddy Muleya.
       </SectionTitle>
       <SectionText>
         A software engineer who constantly seeks out innovative solutions to everyday problems.
         Well-versed in numerous programming languages including C#, JavaScript, PHP OOP, SQL, JAVA, Android Kotlin.
-        I want to be a developer at SovTech because my dream is to be a software develpoer and SovTech is one of the best software 
+        I want to be a developer at SovTech because my dream is to be a software develpoer and SovTech is one of the best software
         development company and the experience i will recieve from SovTech will help me archieve my goals
       </SectionText>
-      <Button onClick={()=>window.location="mailto:freddymuleya16@gmail.com"}>Contact me</Button>
+      <Button onClick={() => window.location = "mailto:freddymuleya16@gmail.com"}>Contact me</Button>
     </LeftSection>
-    
   </Section>
 );
 export default Hero;
